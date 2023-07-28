78a844b7c72487bd695bb6060e5fef273761f1af
SlavikOrl <maks2orlenko@gmail.com>
2023-07-28 16:51:48 +0300

    Base project file

---
+++ StanislavTechnicalGemicle/DescriptionsPages/CompanyCareersPage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalTaskGemicle.DescriptionsPages
+{
+    public class CompanyCareersPage : BasePage
+    {
+        public CompanyCareersPage(IWebDriver driver) : base(driver)
+        {
+        }
+        private static string _openPositionsButtonXpath = "//li[contains(@class, 'buttons-collection-item')]";
+
+        public OpenCareersPage ClickExploreOpenPositionButton(TimeSpan timeToWait = default)
+        {
+            ScrollToElement(200);
+            var openCareerButton = WaitForElementToBeVisible(_openPositionsButtonXpath, timeToWait);
+            openCareerButton.Click();
+            return new OpenCareersPage(driver);
+        }
+    }
+}
